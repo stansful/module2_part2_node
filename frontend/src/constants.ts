85570c052d656fbc24a39afea73d043fe4e97fc9
stansful <stansful@gmail.com>
2022-03-03 12:00:29 +0300

    chore: initialize project
    MODULE-2-PART-2-node

---
+++ frontend/src/constants.ts
@@ -0,0 +1,11 @@
+const API_URL = 'https://hjdjs55gol.execute-api.us-east-1.amazonaws.com/api';
+const PAGE_DID_NOT_EXIST = 'Page didnt exist, we set page to 1';
+const VALIDATION_FAILED = 'Please re-enter password & username';
+const EVENT_TYPES = {
+  submit: 'submit',
+  click: 'click',
+};
+const TOKEN = 'token';
+const TOKEN_EXPIRES_TIME = 'tokenExpiresTime';
+const TOKEN_TIME_TO_LIVE_IN_MINUTES = 10;
+const PAGE = 'page';
