c06eb455752a809c55c6ca69d02016f4c412920c
stansful <stansful@gmail.com>
2022-03-04 11:16:33 +0300

    feat: add some framework interfaces
    add router interface and its dependencies
    
    MODULE-2-PART-2-node

--- backend/src/framework/framework_interfaces.ts
+++ backend/src/framework/framework_interfaces.ts
@@ -1,1 +1,12 @@
+export type HttpMethods = 'GET' | 'POST';
+
 export type VoidHandler = () => void;
+
+export interface RouteMethods {
+  GET?: VoidHandler;
+  POST?: VoidHandler;
+}
+
+export interface Routes {
+  [key: string]: RouteMethods;
+}
