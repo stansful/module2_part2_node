d0363ea41a818357b8be4771ebd522535a765c49
stansful <stansful@gmail.com>
2022-03-04 16:49:37 +0300

    feat: register user router
    MODULE-2-PART-2-node

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import { Application } from './framework/application';
 import { config } from './configs/config';
+import { userRouter } from './user/user_controller';
 
 const app = new Application();
-const PORT = config.PORT;
+const PORT = config.env.PORT;
+
+app.registerRouter(userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started http://localhost:${PORT}`);
