93cfdf4414c297ffbf9093b6fe0f89cddeba63fa
stansful <stansful@gmail.com>
2022-03-04 18:56:53 +0300

    feat: add notFount method
    MODULE-2-PART-2-node

--- backend/src/response/response_service.ts
+++ backend/src/response/response_service.ts
@@ -15,6 +15,13 @@ class ResponseService {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(token));
   }
+
+  public notFound(res: ServerResponse) {
+    const notFoundMessage = { message: 'Not Found' };
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(notFoundMessage));
+  }
 }
 
 export const responseService = new ResponseService();
