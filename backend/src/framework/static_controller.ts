afaa7eeb1b2309066c46e956e17f23b57198a198
stansful <stansful@gmail.com>
2022-03-08 23:44:38 +0300

    feat: update static controller
    add routes for html, css, js, pictures folders
    
    MODULE-2-PART-2-node

--- backend/src/framework/static_controller.ts
+++ backend/src/framework/static_controller.ts
@@ -1,6 +1,8 @@
 import { Router } from './router';
 import path from 'path';
 import fs from 'fs';
+import { staticService } from './static_service';
+import { config } from '../configs/config';
 
 const staticRouter = new Router();
 
@@ -18,5 +20,36 @@ const staticConfig = {
     frontend: fs.readdirSync(path.resolve(__dirname, '..', '..', 'static', 'frontend')),
   },
 };
+
+staticService.addStaticRoutes(
+  staticConfig.files.pictures,
+  staticConfig.path.pictures,
+  staticRouter,
+  staticService.streamHandler,
+  config.mimeTypes.HTML,
+);
+staticService.addStaticRoutes(
+  staticConfig.files.css,
+  staticConfig.path.css,
+  staticRouter,
+  staticService.fileHandler,
+  config.mimeTypes.CSS,
+  '/css',
+);
+staticService.addStaticRoutes(
+  staticConfig.files.js,
+  staticConfig.path.js,
+  staticRouter,
+  staticService.fileHandler,
+  config.mimeTypes.JS,
+  '/js',
+);
+staticService.addStaticRoutes(
+  staticConfig.files.frontend.filter((file) => file !== 'js' && file !== 'css'),
+  staticConfig.path.frontend,
+  staticRouter,
+  staticService.fileHandler,
+  config.mimeTypes.HTML,
+);
 
 export { staticRouter };
