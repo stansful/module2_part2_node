d9b2f9ae11bcb1f162ec51bc0b760b9ef627bb30
stansful <stansful@gmail.com>
2022-03-08 23:54:01 +0300

    feat: update gallery service
    update findAll method. Its validate user, requested page, send gallery objects if valid, else response with unauthorize or bad request messages
    
    MODULE-2-PART-2-node

--- backend/src/gallery/gallery_service.ts
+++ backend/src/gallery/gallery_service.ts
@@ -1,10 +1,43 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import { config } from '../configs/config';
+import { responseService } from '../response/response_service';
+import { Gallery } from './gallery_interface';
+import { tokenService } from '../token/token_service';
 
 class GalleryService {
-  findAll(req: IncomingMessage, res: ServerResponse, url: URL) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('objects:string[], page:string');
+  public async findAll(req: IncomingMessage, res: ServerResponse, url: URL) {
+    const token = req.headers.authorization || '';
+    const isValid = tokenService.validateToken(token);
+
+    if (!isValid) {
+      return responseService.unauthorized(res);
+    }
+
+    const limit = config.DEFAULT_PICTURE_LIMIT;
+    const page = Number(url.searchParams.get('page')) || 1;
+
+    const picturesPath = path.resolve(__dirname, '..', '..', 'static', 'pictures');
+    const allPictures = await fs.readdir(picturesPath);
+
+    const totalPages = Math.ceil(allPictures.length / limit);
+
+    if (page > totalPages || page < 0) {
+      return responseService.badRequest(res, totalPages);
+    }
+
+    const offset = page === 1 ? page : (page - 1) * limit;
+    const start = page === 1 ? 0 : offset;
+    const end = start + limit;
+
+    const requiredPictures = allPictures
+      .slice(start, end)
+      .map((picture) => (picture = `${config.env.PROTOCOL}://${config.env.DOMAIN}:${config.env.PORT}/${picture}`));
+
+    const galleryObjects: Gallery = { objects: requiredPictures, page, total: totalPages };
+
+    responseService.galleryObjects<Gallery>(res, galleryObjects);
   }
 }
 
