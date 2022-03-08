b58841efcbce5e47207a60207ee6b6c8ce4fae3b
stansful <stansful@gmail.com>
2022-03-08 13:24:08 +0300

    feat: add http status codes
    MODULE-2-PART-2-node

--- backend/src/configs/config.ts
+++ backend/src/configs/config.ts
@@ -6,6 +6,12 @@ export const config: Config = {
     PROTOCOL: process.env.PROTOCOL || 'http',
     DOMAIN: process.env.DOMAIN || 'localhost',
   },
+  httpStatusCodes: {
+    OK: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+  },
   DEFAULT_LIMIT: 5,
   SECRET_AUTHORIZATION_TOKEN: 'token',
 };
--- backend/src/configs/config_interface.ts
+++ backend/src/configs/config_interface.ts
@@ -4,6 +4,12 @@ export interface Config {
     PROTOCOL: string;
     DOMAIN: string;
   };
+  httpStatusCodes: {
+    OK: number;
+    BAD_REQUEST: number;
+    UNAUTHORIZED: number;
+    NOT_FOUND: number;
+  };
   DEFAULT_LIMIT: number;
   SECRET_AUTHORIZATION_TOKEN: string;
 }
