615f3a95c9ef5f14117450c48a62dea1eee10d2d
stansful <stansful@gmail.com>
2022-03-09 18:48:55 +0300

    feat: edit pictures mime type to binary type
    MODULE-2-PART-2-node

--- backend/src/framework/static_controller.ts
+++ backend/src/framework/static_controller.ts
@@ -26,7 +26,7 @@ staticService.addStaticRoutes(
   staticConfig.path.pictures,
   staticRouter,
   staticService.streamHandler,
-  config.mimeTypes.JPEG,
+  config.mimeTypes.BIN,
 );
 staticService.addStaticRoutes(
   staticConfig.files.css,
